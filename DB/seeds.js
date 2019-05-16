d260b63d9513ecf11f641bd3dfa2bc93a0f9e16b
Branden White <Brandenjwhite@gmail.com>
2019-05-16 12:34:45 -0400

    added code to seeds file

---
+++ DB/seeds.js
@@ -0,0 +1,18 @@
+let Mural = [
+    {
+location: "Atlanta Beltline",
+artist: "Brandon Sadler",
+installation: "Graffiti Art",
+Date: "01/05/2019"
+}
+]
+
+[
+    {
+        location: "Eastside Trail Beltline",
+        artist: "Big Teef",
+        installation: "Graffiti art",
+        Date: "06/21/2019"
+    }
+]
+
